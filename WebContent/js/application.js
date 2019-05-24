d769a6a94fd4805e8dfeac3ea84976f1bd7014d2
Ludivine <ludivinelanclume@gmail.com>
2019-05-24 11:04:35 +0200

    magasin et produit html

--- WebContent/js/application.js
+++ WebContent/js/application.js
@@ -1,14 +1,32 @@
-function chargerPersonnes() {
-	$.getJSON("http://localhost:8080/ProjetEjbWs/service/personnes", function(data) {
+function chargerMagasins() {
+	$.getJSON("localhost:8080/synthese-serveur-ejb-ws-equipe-1/produits/getAllMagasins", function(data) {
 		$("#tableBody").html("");
 		
 		for(i in data) {
 			$tr = $("<tr>");
 			
-			$td = $("<td>").append(data[i]['id']);$tr.append($td);
-			$td = $("<td>").append(data[i]['nom']);$tr.append($td);
-			$td = $("<td>").append(data[i]['prenom']);$tr.append($td);
-			$td = $("<td>").append(data[i]['age']);$tr.append($td);
+			$td = $("<td>").append(data[i]['idMagasin']);$tr.append($td);
+			$td = $("<td>").append(data[i]['nomMagasin']);$tr.append($td);
+			$td = $("<td>").append(data[i]['codeMagasin']);$tr.append($td);
+			$td = $("<td>").append(data[i]['prixDuLocal']);$tr.append($td);
+			
+			
+			$("#tableBody").append($tr);
+		}
+	});
+}
+
+function chargerProduits() {
+	$.getJSON("localhost:8080/synthese-serveur-ejb-ws-equipe-1/produits/getAllProduits", function(data) {
+		$("#tableBody").html("");
+		
+		for(i in data) {
+			$tr = $("<tr>");
+			
+			$td = $("<td>").append(data[i]['idProduit']);$tr.append($td);
+			$td = $("<td>").append(data[i]['nomProduit']);$tr.append($td);
+			$td = $("<td>").append(data[i]['stock']);$tr.append($td);
+			$td = $("<td>").append(data[i]['prix']);$tr.append($td);
 			
 			
 			$("#tableBody").append($tr);
